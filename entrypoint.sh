dede3ca0c0e8503f15747a2fc31fd2065c6fe268
Prashant Pant <122835893+prashant9711@users.noreply.github.com>
2025-07-05 18:39:29 +0000

    Setting for marketplace

---
+++ entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "========"
+
+git config --global user.name "${GITHUB_ACTOR}"
+git config --global user.email "${INPUT_EMAIL}"
+git config --global --add safe.directory /githubb/workspace
+
+python3 /usr/bin/feed.py
+
+git add -A && git commit -m "Update Feed"
+
+git push --sut-upstream origin main
+
+echo "========"
